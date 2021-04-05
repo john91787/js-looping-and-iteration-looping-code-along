12d72e698edbd9a6eee79af8f75ebc54cce800e5
jonathan <jhector39@gmail.com>
2021-04-05 11:28:47 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+const event = [`surprise`];
+let giveMessage = []
+function writeCards(names, event){
+    for (let i=0; i<names.length; i++){
+        giveMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+    return giveMessage;
+}
+const callCount = [0];
+function countDown(callCount){
+    while (callCount > 0 ){
+        console.log( callCount);
+        callCount -=1;
+    }
+    console.log(callCount);
+}
